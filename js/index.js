e03c77d0b1b2628f8e371c3431a3dac817e5efeb
lizvinskyi <andriilizvinskyi@gmail.com>
2024-02-12 15:02:34 +0200

    fixes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-console.log('Hello World');
+$('.restaurant-wrap-slider-init').slick({
+    dots: true,
+    infinite: true,
+    arrows: false,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+});
